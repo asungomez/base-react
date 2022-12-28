e8058e86f3dfc740192f8e019da50fa64bc47bf1
Maria.GomezCastro <Maria.GomezCastro@eu.jll.com>
2022-12-28 15:50:26 +0100

    Add Amplify config

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
 import "./index.css";
+import { Amplify } from "aws-amplify";
+import AWSConfig from "./aws-exports";
+
+Amplify.configure(AWSConfig);
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
