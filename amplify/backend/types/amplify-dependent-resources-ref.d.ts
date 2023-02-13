7baf430ccf3e5abf0c14beecd903af39aa45efa6
Maria.GomezCastro <Maria.GomezCastro@eu.jll.com>
2023-02-13 15:31:58 +0100

    Add custom message for create account

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,22 +1,22 @@
 export type AmplifyDependentResourcesAttributes = {
-  auth: {
-    authentication: {
-      AppClientID: "string";
-      AppClientIDWeb: "string";
-      IdentityPoolId: "string";
-      IdentityPoolName: "string";
-      UserPoolArn: "string";
-      UserPoolId: "string";
-      UserPoolName: "string";
-    };
-  };
-  function: {
-    authenticationCustomMessage: {
-      Arn: "string";
-      LambdaExecutionRole: "string";
-      LambdaExecutionRoleArn: "string";
-      Name: "string";
-      Region: "string";
-    };
-  };
-};
+  "auth": {
+    "authentication": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "authenticationCustomMessage": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
