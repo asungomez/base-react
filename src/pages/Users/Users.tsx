c4ab10a99bdcc4bd5bb1aa6e9758ff32caa7f24d
Maria.GomezCastro <Maria.GomezCastro@eu.jll.com>
2023-04-10 14:21:39 +0200

    Unit test async responses

--- src/pages/Users/Users.tsx
+++ src/pages/Users/Users.tsx
@@ -4,22 +4,34 @@ import { UsersTable } from "../../components/UsersTable/UsersTable";
 import { getUsers, User } from "../../services/authentication";
 import AddIcon from "@mui/icons-material/Add";
 import { useNavigate } from "react-router-dom";
+import { ErrorCode } from "../../services/error";
+import { Error } from "../../components/Error/Error";
 
 export const UsersPage: FC = () => {
   const [loading, setLoading] = useState(true);
   const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState<ErrorCode | null>(null);
   const navigate = useNavigate();
 
   const toCreateUser = () => navigate("/users/create");
 
   useEffect(() => {
     if (loading) {
-      getUsers().then((users) => {
-        setLoading(false);
-        setUsers(users);
-      });
+      setError(null);
+      getUsers()
+        .then((users) => {
+          setLoading(false);
+          setUsers(users);
+        })
+        .catch((error) => {
+          setError(error);
+        });
     }
   }, [loading, setUsers, setLoading]);
+
+  if (error) {
+    return <Error code={error} />;
+  }
   return (
     <>
       <Typography variant="h3" gutterBottom>
