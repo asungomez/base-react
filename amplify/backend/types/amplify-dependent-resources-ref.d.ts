1732a3febc8054ddd73f143a553cde69738b6097
Maria Gomez Castro <maria.gomezcastro@jll.com>
2023-09-28 12:50:37 +0200

    Lint and format

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,61 +1,61 @@
 export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "AdminQueries": {
-      "ApiId": "string",
-      "ApiName": "string",
-      "RootUrl": "string"
-    },
-    "dataapi": {
-      "ApiId": "string",
-      "ApiName": "string",
-      "RootUrl": "string"
-    }
-  },
-  "auth": {
-    "auth": {
-      "AppClientID": "string",
-      "AppClientIDWeb": "string",
-      "IdentityPoolId": "string",
-      "IdentityPoolName": "string",
-      "UserPoolArn": "string",
-      "UserPoolId": "string",
-      "UserPoolName": "string"
-    },
-    "userPoolGroups": {
-      "AdminGroupRole": "string"
-    }
-  },
-  "function": {
-    "AdminQueries56514224": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "LambdaExecutionRoleArn": "string",
-      "Name": "string",
-      "Region": "string"
-    },
-    "authCustomMessage": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "LambdaExecutionRoleArn": "string",
-      "Name": "string",
-      "Region": "string"
-    },
-    "dataapi": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "LambdaExecutionRoleArn": "string",
-      "Name": "string",
-      "Region": "string"
-    }
-  },
-  "storage": {
-    "exercises": {
-      "Arn": "string",
-      "Name": "string",
-      "PartitionKeyName": "string",
-      "PartitionKeyType": "string",
-      "Region": "string",
-      "StreamArn": "string"
-    }
-  }
-}
+  api: {
+    AdminQueries: {
+      ApiId: "string";
+      ApiName: "string";
+      RootUrl: "string";
+    };
+    dataapi: {
+      ApiId: "string";
+      ApiName: "string";
+      RootUrl: "string";
+    };
+  };
+  auth: {
+    auth: {
+      AppClientID: "string";
+      AppClientIDWeb: "string";
+      IdentityPoolId: "string";
+      IdentityPoolName: "string";
+      UserPoolArn: "string";
+      UserPoolId: "string";
+      UserPoolName: "string";
+    };
+    userPoolGroups: {
+      AdminGroupRole: "string";
+    };
+  };
+  function: {
+    AdminQueries56514224: {
+      Arn: "string";
+      LambdaExecutionRole: "string";
+      LambdaExecutionRoleArn: "string";
+      Name: "string";
+      Region: "string";
+    };
+    authCustomMessage: {
+      Arn: "string";
+      LambdaExecutionRole: "string";
+      LambdaExecutionRoleArn: "string";
+      Name: "string";
+      Region: "string";
+    };
+    dataapi: {
+      Arn: "string";
+      LambdaExecutionRole: "string";
+      LambdaExecutionRoleArn: "string";
+      Name: "string";
+      Region: "string";
+    };
+  };
+  storage: {
+    exercises: {
+      Arn: "string";
+      Name: "string";
+      PartitionKeyName: "string";
+      PartitionKeyType: "string";
+      Region: "string";
+      StreamArn: "string";
+    };
+  };
+};
