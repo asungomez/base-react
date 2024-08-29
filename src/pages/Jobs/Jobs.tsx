1c3959a804f7aeaae02e26cd620c96dd9531f674
Asun Gomez Castro <agcastro7@gmail.com>
2024-08-29 13:07:36 +0200

    Add navigation arrows

--- src/pages/Jobs/Jobs.tsx
+++ src/pages/Jobs/Jobs.tsx
@@ -1,26 +1,54 @@
-import { Button, Typography } from "@mui/material";
-import { FC } from "react";
+import { Button, Stack, Typography } from "@mui/material";
+import { FC, useState } from "react";
 import { DailyJobs } from "../../components/DailyJobs/DailyJobs";
 import dayjs from "dayjs";
 import { Link } from "react-router-dom";
 import AddIcon from "@mui/icons-material/Add";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 export const JobsPage: FC = () => {
-  const date = dayjs().format("YYYY-MM-DD");
+  const initialDate = dayjs();
+  const [date, setDate] = useState(initialDate);
+  const dateString = date.format("YYYY-MM-DD");
+  const title = date.format("dddd, MMMM D, YYYY");
+
+  const previousDayHandler = () => {
+    setDate((date) => date.subtract(1, "day"));
+  };
+
+  const nextDayHandler = () => {
+    setDate((date) => date.add(1, "day"));
+  };
   return (
     <>
       <Typography variant="h3" gutterBottom>
         Jobs
       </Typography>
-      <Typography variant="h4" sx={{ mb: "20px" }}>
-        Today&apos;s jobs
-      </Typography>
-      <Link to={`/jobs/create?date=${date}`}>
+      <Stack direction="row" spacing={2} sx={{ mb: "20px" }}>
+        <Button
+          variant="outlined"
+          onClick={previousDayHandler}
+          startIcon={<ArrowBackIosIcon />}
+        >
+          Previous
+        </Button>
+        <Typography variant="h4">{title}</Typography>
+        <Button
+          variant="outlined"
+          onClick={nextDayHandler}
+          endIcon={<ArrowForwardIosIcon />}
+        >
+          Next
+        </Button>
+      </Stack>
+
+      <Link to={`/jobs/create?date=${dateString}`}>
         <Button variant="outlined" startIcon={<AddIcon />} sx={{ mb: "20px" }}>
           Add new
         </Button>
       </Link>
-      <DailyJobs date={date} />
+      <DailyJobs date={dateString} />
     </>
   );
 };
